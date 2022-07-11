f19c54c47e50ff3d59c2628d0c67b7bb02763ae0
Bert Joseph Prestoza <bertjosephprestoza@gmail.com>
2022-07-10 22:12:50 -0700

    Add game functions

---
+++ script.js
@@ -0,0 +1,81 @@
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let computerSelection;
+    switch (randomNumber) {
+        case 1:
+            computerSelection = "Rock";
+            break;
+        case 2:
+            computerSelection = "Paper";
+            break;
+        case 3:
+            computerSelection = "Scissors";
+            break;
+        default:
+            computerSelection = "Error";
+    }
+    return computerSelection;
+}
+
+function capitalize(str) {
+    let strLower = str.toLowerCase();
+
+    let strFirstLetter = strLower.slice(0, 1);
+    strFirstLetter = strFirstLetter.toUpperCase();
+
+    let strRestOfLetters = strLower.slice(1);
+
+    return strFirstLetter + strRestOfLetters;
+}
+
+function playerPlay() {
+    let playerSelection;
+    let validChoice = true;
+    while (validChoice) {
+        playerSelection = prompt("Enter your choice (rock, paper, scissors): ");
+        playerSelection = capitalize(playerSelection);
+
+        if (
+            (playerSelection === "Rock") ||
+            (playerSelection === "Paper") ||
+            (playerSelection === "Scissors")
+        ) {
+            validChoice = false;
+        }
+    }
+    return playerSelection;
+}
+
+function displaySelection(playerSelection, computerSelection) {
+    console.log(`You: ${playerSelection}   vs.   Computer: ${computerSelection}`);
+}
+
+function playRound(playerSelection, computerSelection) {
+    displaySelection(playerSelection, computerSelection);
+    if (
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else if (
+        (playerSelection === "Rock" && computerSelection === "Paper") ||
+        (playerSelection === "Paper" && computerSelection === "Scissors") ||
+        (playerSelection === "Scissors" && computerSelection === "Rock")
+    ) {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (
+        (playerSelection === "Rock" && computerSelection === "Rock") ||
+        (playerSelection === "Paper" && computerSelection === "Paper") ||
+        (playerSelection === "Scissors" && computerSelection === "Scissors")
+    ) {
+        return "It's a tie!";
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(playerPlay(), computerPlay()));
+    }
+}
+
